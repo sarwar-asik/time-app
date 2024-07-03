979c6259d37e5ce27612ac3a444bd230a5d69c6d
sarwar-asik <sarwarasik@gmail.com>
2024-07-03 14:45:22 +0600

    set-up(google-font): installed google font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
+import { Inter_200ExtraLight, Inter_900Black, useFonts, } from '@expo-google-fonts/inter';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  let [fontLoaded] = useFonts({
+    inter_font_900: Inter_900Black,
+    inter_font: Inter_200ExtraLight
+  })
+  if (!fontLoaded) {
+    return null
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up the Clock App!</Text>
+      <View>
+        <Text style={{ fontFamily: "inter_font", fontSize: 30 }}>This is inter_font Font</Text>
+        <Text style={{ fontFamily: "inter_font_900", fontSize: 30 }}>This is inter_font_900 Font</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
