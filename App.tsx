9c8b71eef64c94a0c0d99dd20d91d619bccbef02
sarwar-asik <sarwarasik@gmail.com>
2024-07-03 14:54:35 +0600

    features(bg-section): ImageBackground added in page

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { Inter_200ExtraLight, Inter_900Black, useFonts, } from '@expo-google-fonts/inter';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   let [fontLoaded] = useFonts({
@@ -11,14 +11,9 @@ export default function App() {
     return null
   }
   return (
-    <View style={styles.container}>
-      <Text>Open up the Clock App!</Text>
-      <View>
-        <Text style={{ fontFamily: "inter_font", fontSize: 30 }}>This is inter_font Font</Text>
-        <Text style={{ fontFamily: "inter_font_900", fontSize: 30 }}>This is inter_font_900 Font</Text>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={require("./assets/app/light-bg.png")} style={{ flex: 1 }}>
+
+    </ImageBackground>
   );
 }
 
